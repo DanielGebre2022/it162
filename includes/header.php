a8209bde92d2c1353becd508a90d5e4277fe6baf
leteletelete <danielgebreselassie20@gmail.com>
2022-01-18 16:21:54 -0800

    first commit

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>IT162 Portal - Edited it and made it my own</title>
+<meta name="viewport" content="width=device-width" />
+<meta name="robots" content="noindex,nofollow" />
+<meta charset="utf-8">
+<script src="https://use.fontawesome.com/6a71565c22.js"></script>
+<link rel="stylesheet" href="css/nav.css" />
+<link rel="stylesheet" href="css/portal.css" />
+<link rel="stylesheet" href="css/form.css" />
+</head>
+
+<body>
+<!-- START WRAPPER -->
+<main class="wrapper">
+<!-- header ends here -->

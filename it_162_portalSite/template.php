0b09656a059453aa93ce68e08020d7fee99e47cd
DanielGebre2022 <91226945+DanielGebre2022@users.noreply.github.com>
2022-01-19 16:47:21 -0800

    Updated template.php

--- it_162_portalSite/template.php
+++ it_162_portalSite/template.php
@@ -1,34 +1,16 @@
 <?php include 'includes/header.php';?>
-<header>
-  <h1><a href="index.php"><i class="logo fa fa-home"></i> Daniel Gebreselassie Winter 2022 SCC IT162 Portal</a></h1>
-  <nav>
-    <ul class="topnav" id="myTopnav">
-      <li><a href="/index.php" class="selected">Welcome</a></li>
-      <li><a href="/big/index.php">Big</a></li>
-      <li><a href="aia.php">AIA</a></li>
-      <li><a href="flowchart.php">Flowchart/Layout</a></li>
-      <li><a href="/fp/index.php">Final Project</a></li>
-      <li><a href="contactme.php">Contact Sara</a></li>
-      <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
-    </ul>
-  </nav>
-</header>
-        
-<!-- START LEFT COL -->
-<section>
- <h2 class="pageID">Template</h2>
-
 <!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
- <img src="images/IMG_0758.jpeg" class="desktop" alt="" />
- <img src="images/IMG_0887.JPG" class="phone" alt="" />
-<p>Template</p>
+<section>
+ <h1>Template</h1>
+ <p>Template</p>
+
 </section>
 <!-- END LEFT COL -->
 
 <!-- START RIGHT COL -->
 <aside>
- <h3>Right Column</h3>
-
+ <h3 class="display-3">Teamwork</h3>
+ <img src="images/IMG_0620.WEBP" class="tablet" alt="" />
 </aside>
 <!-- END RIGHT COL -->
  
